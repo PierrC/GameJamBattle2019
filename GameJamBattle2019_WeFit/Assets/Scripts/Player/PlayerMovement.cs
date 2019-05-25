2b5d18efd3082521c1ad5b5146a83061d4d0095f
XakuX <hstindel@wanadoo.fr>
2019-05-25 10:39:21 -0400

    Controller player basic

--- GameJamBattle2019_WeFit/Assets/Scripts/Player/PlayerMovement.cs
+++ GameJamBattle2019_WeFit/Assets/Scripts/Player/PlayerMovement.cs
@@ -49,11 +49,14 @@ public class PlayerMovement : MonoBehaviour
 
     private IEnumerator MovingTo(Tube tube)
     {
-        while (Vector2.Distance(this.position2D,tube.position2D) > 1)
+        while (Vector2.Distance(this.position2D,tube.position2D) > 0.1f)
         {
-            this.GetComponent<Rigidbody>().AddForce(tube.position2D - this.position2D);
+            this.GetComponent<Rigidbody>().AddForce(5*(tube.position2D - this.position2D));
             yield return null;
         }
+        this.actualTube = tube;
+        this.transform.position = new Vector3(actualTube.position2D.x, actualTube.position2D.y, this.transform.position.z);
+        this.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, this.GetComponent<Rigidbody>().velocity.z);
         moving = false;
     }
 }

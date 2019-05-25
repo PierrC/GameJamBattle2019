76d1e5ac78b6eb51ab125dccc78540c713b9667f
Nehapolitan <46408046+Nehapolitan@users.noreply.github.com>
2019-05-25 17:45:19 -0400

    re state test wwise

---
+++ GameJamBattle2019_WeFit/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,68 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID L1 = 1702304824U;
+        static const AkUniqueID L2 = 1702304827U;
+        static const AkUniqueID L3 = 1702304826U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace LAYERS
+        {
+            static const AkUniqueID GROUP = 3298531235U;
+
+            namespace STATE
+            {
+                static const AkUniqueID DRUM_00 = 513591012U;
+                static const AkUniqueID L1 = 1702304824U;
+                static const AkUniqueID L1ANDL2 = 2760178885U;
+                static const AkUniqueID L1ANDL2ANDL3 = 599649471U;
+                static const AkUniqueID L1ANDL2ANDL3ANDL4 = 1468994450U;
+                static const AkUniqueID L1ANDL2ANDL4 = 599649464U;
+                static const AkUniqueID L1ANDL3 = 2760178884U;
+                static const AkUniqueID L1ANDL4 = 2760178883U;
+                static const AkUniqueID L2 = 1702304827U;
+                static const AkUniqueID L2ANDL3 = 3116499169U;
+                static const AkUniqueID L2ANDL3ANDL4 = 714328492U;
+                static const AkUniqueID L2ANDL4 = 3116499174U;
+                static const AkUniqueID L3 = 1702304826U;
+                static const AkUniqueID L3ANDL4 = 3533580213U;
+                static const AkUniqueID L4 = 1702304829U;
+            } // namespace STATE
+        } // namespace LAYERS
+
+    } // namespace STATES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID TEST = 3157003241U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__

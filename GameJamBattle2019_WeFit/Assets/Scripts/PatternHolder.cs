fca0bb6c3132ae63f654fbfbcee4e75b7e1331ef
MlleEmcy <mc.richer@hotmail.com>
2019-05-25 18:32:35 -0400

    dictionnaire

---
+++ GameJamBattle2019_WeFit/Assets/Scripts/PatternHolder.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatternHolder
+{
+    Dictionary<float, int[]> example = new Dictionary<float, int[]>()
+    {
+        {0.5f, new int [3] {0,1,5}},
+        {1f, new int [3] {0,2,4}},
+        {1.5f, new int [3] {1,3,5}},
+        {2f, new int [2] {3,4}},
+        {1.5f, new int [3] {0,3,5}},
+    };
+
+
+
+}

9bfb86bf7927d634c5f8836a2428a278a10e8558
Pierre Cudennec <pierre.yves.cudennec@gmai.com>
2019-05-25 18:53:05 -0400

    Pattern maker tested

--- GameJamBattle2019_WeFit/Assets/Scripts/PatternHolder.cs
+++ GameJamBattle2019_WeFit/Assets/Scripts/PatternHolder.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PatternHolder
+public static class PatternHolder
 {
-    Dictionary<float, int[]> example = new Dictionary<float, int[]>()
+
+    public static SortedList<float, int[]> example = new SortedList<float, int[]>()
     {
         {0.5f, new int [3] {0,1,5}},
         {1f, new int [3] {0,2,4}},
         {1.5f, new int [3] {1,3,5}},
         {2f, new int [2] {3,4}},
-        {1.5f, new int [3] {0,3,5}},
+        {2.5f, new int [3] {0,3,5}},
     };
 
 

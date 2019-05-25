f7167fb423ee5df1c9798ff29f6a540df903f463
XakuX <hstindel@wanadoo.fr>
2019-05-24 23:44:27 -0400

    Scene hugo initalisé

---
+++ GameJamBattle2019_WeFit/Assets/Scripts/Camera/ConnectCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ConnectCamera : MonoBehaviour
+{
+    public GameObject attachObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

8f0b2dde9358fd01cc41d1dfc1af7b44a2d943a8
MlleEmcy <mc.richer@hotmail.com>
2019-05-25 19:20:19 -0400

    pattern1

--- GameJamBattle2019_WeFit/Assets/Scripts/PatternHolder.cs
+++ GameJamBattle2019_WeFit/Assets/Scripts/PatternHolder.cs
@@ -7,11 +7,11 @@ public static class PatternHolder
 
     public static SortedList<float, int[]> example = new SortedList<float, int[]>()
     {
-        {0.5f, new int [3] {0,1,5}},
-        {1f, new int [3] {0,2,4}},
-        {1.5f, new int [3] {1,3,5}},
-        {2f, new int [2] {3,4}},
-        {2.5f, new int [3] {0,3,5}},
+        {0.5f, new int [2] {3,4}},
+        {1f, new int [0] {}},
+        {1.5f, new int [3] {1,2,3}},
+        {2f, new int [0] {}},
+        {2.5f, new int [3] {1,4,5}},
     };
 
 

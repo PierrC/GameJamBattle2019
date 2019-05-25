c72b9beeb8140d2821754ed5b1b0adaafbc9ddf4
XakuX <hstindel@wanadoo.fr>
2019-05-25 19:27:14 -0400

    MainMenu to main scene to victoryScreen to mainMenu

---
+++ GameJamBattle2019_WeFit/Assets/Scripts/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public float totalTime;
+    private float actualTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        actualTime = 0;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        actualTime += Time.deltaTime;
+        if(actualTime > totalTime)
+        {
+            SceneManager.LoadScene("VictoryScreen");
+        }
+    }
+}

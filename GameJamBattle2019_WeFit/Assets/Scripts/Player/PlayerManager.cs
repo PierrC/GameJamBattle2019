b1fedd76dd14632c40ed87b3b377e17c2c8a3ed4
XakuX <hstindel@wanadoo.fr>
2019-05-25 12:09:34 -0400

    Health beginning

---
+++ GameJamBattle2019_WeFit/Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public List<int> instruments;
+    public int maxLife;
+
+    private List<bool> instrumentsMax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instrumentsMax = new List<bool>();
+        for(int i = 0; i < instruments.Count; i++)
+        {
+            instrumentsMax.Add(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0; i < instruments.Count; i++)
+        {
+            if(instruments[i] == maxLife && !instrumentsMax[i])
+            {
+                
+            }
+        }
+    }
+}
